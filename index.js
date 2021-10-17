a09f91f38c4ea3afbef63ded5282d47fc9347473
ezeilo-su <ezeilo.su@gmail.com>
2021-10-17 19:03:15 +0100

    add moderation service

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const morgan = require('morgan');
+const { json, urlencoded } = express;
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(4003, () => {
+  console.log('Listening on port 4003');
+});
